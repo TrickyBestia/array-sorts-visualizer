fada8d83ac26d32972f4f3f2a22c48f5ed6d322d
TrickyBestia <trickybestia@gmail.com>
2021-02-28 09:40:49 +0300

    Fix snippet code formatting

--- src/components/CodeEditor/snippets.ts
+++ src/components/CodeEditor/snippets.ts
@@ -6,23 +6,23 @@ const snippets: { name: string; value: string }[] = [
   {
     name: 'bubble sort',
     value: `function* sort(arrayProxy) {
-    for (let i = 0; i < arrayProxy.array.length - 1; i++) {
-      let wasSwap = false;
-      for (let j = 0; j < arrayProxy.array.length - 1 - i; j++) {
-        arrayProxy.primaryIndex = j;
+  for (let i = 0; i < arrayProxy.array.length - 1; i++) {
+    let wasSwap = false;
+    for (let j = 0; j < arrayProxy.array.length - 1 - i; j++) {
+      arrayProxy.primaryIndex = j;
+      yield undefined;
+      arrayProxy.secondaryIndex = j + 1;
+      yield undefined;
+      if (arrayProxy.compare() > 0) {
         yield undefined;
-        arrayProxy.secondaryIndex = j + 1;
-        yield undefined;
-        if (arrayProxy.compare() > 0) {
-          yield undefined;
-          arrayProxy.swap();
-          wasSwap = true;
-        }
-        yield undefined;
+        arrayProxy.swap();
+        wasSwap = true;
       }
-      if (!wasSwap) break;
+      yield undefined;
     }
-  }`,
+    if (!wasSwap) break;
+  }
+}`,
   },
 ];
 export default snippets;
